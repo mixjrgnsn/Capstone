c3672fe3636fd471ab7e66856bc4586cb5a8005d
Mico Jorgensen <jorgensenmico@gmail.com>
2024-10-13 13:00:51 +0800

    logout

---
+++ web/JS/logout.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    if (!localStorage.getItem('userData')) {
+        // Redirect to login if there's no user data
+        window.location.replace('../HTML/index.html');
+    }
+};
