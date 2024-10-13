5a608ec16d57b6789578c76e757dcde3e86bb213
Mico Jorgensen <jorgensenmico@gmail.com>
2024-10-13 14:04:32 +0800

    wewe

--- web/JS/logout.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    if (!localStorage.getItem('userData')) {
-        // Redirect to login if there's no user data
-        window.location.replace('../HTML/index.html');
-        history.replaceState(null, '', '../HTML/index.html');
-    }
-};
